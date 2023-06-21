2a9b3fc201cdce02a618cabcdc2feeff2f84d730
nam2911 <namiuh96@gmail.com>
2023-06-21 21:05:06 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Baitap1 from './BaiTapThucHanhLayout';
+
+function App() {
+  return (
+    <div><Baitap1/></div>
+  );
+}
+
+export default App;
---
+++ src/BaiTapThucHanhLayout/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './navbar';
+import Header from './header';
+import content from './content';
+import footer from './footer';
+
+class Baitap1 extends Component {
+    render() {
+        return (
+            <Navbar />
+        )
+    }
+}
+
+export default Baitap1;
